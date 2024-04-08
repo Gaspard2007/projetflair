4e1b729d412f86ecedd62eab03fbe2b86d896c06
Gaspard2007 <124401506+Gaspard2007@users.noreply.github.com>
2024-04-08 21:19:30 +0000

    Partie de gaspard

--- components/proposition-business-booster.tsx
+++ components/proposition-business-booster.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Link from 'next/link'
+
 const PropositionLocation: React.FC = () => {
     const [showInput, setShowInput] = useState(false);
 
@@ -8,12 +8,31 @@ const PropositionLocation: React.FC = () => {
     };
 
     return (
-        <div>
-            <div className='flex'>
-            <img style={{width:''}} src="/imagepourshop.svg" alt="" />
-            <h3>BUSINESS BOOSTER 1H POUR CHANGER DE VIE</h3>
+        <div className='w-[85%] flex ' style={{background:'#DEDEDE'}}>
+            <img style={{height:'80%', width:'80%'}} src="/imagepourshop.png" alt="" />
+            <div className='flex flex-col justify-between' style={{margin:'30px'}}>
+                <div>
+                    <h3>BUSINESS BOOSTER 1H POUR CHANGER DE VIE</h3>
+                    <div style={{fontWeight:'700px'}}>
+                        <span className=''>100€</span>
+                    </div>
+                    <p>Vous vous sentez perdue face à la multitude de formations esthétique existantes ?</p>
+                    <p>Notre session de coaching intensif est conçue pour vous orienter vers la formation en esthétique la plus adaptée à vos ambitions.</p>
+                    <p>En deux heures, nous explorerons ensemble vos passions, vos compétences et déterminerons la voie qui vous correspond le mieux</p>            
+                </div>
+                <div className='flex flex-col items-end'>
+                    <button onClick={toggleInput} style={{padding:'11px', width:'50%'}} className='flex items-center text-white text-lg cursor-pointer bg-black'>
+                        <img src="/calendar.svg" style={{marginRight:'20px'}} alt="" />
+                        CHOISIR UNE DATE
+                    </button>
+                    {showInput && <input style={{width:'50%'}} type="date" />}
+                </div>
+                <br />
+                <div className='flex items-center justify-end'>
+                    <button style={{padding:'11px', width:'50%'}} className='text-white text-lg cursor-pointer bg-black'>JE RÉSERVE</button>
+                </div>
             </div>
-            </div>
+        </div>
     );
 }
 
