3a0b9baa4df7c1fe873c087f6b2cd718b4242df5
Gaspard2007 <124401506+Gaspard2007@users.noreply.github.com>
2024-04-08 05:58:12 +0000

    ajout de partie de Gaspard

--- app/dashboard_user/page.tsx
+++ app/dashboard_user/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { Button } from 'antd';
-import MenuParticulier from "@/components_récurent/menu-particulier";
-import MenuDashboardParticulier from "@/components_récurent/menu-dashboard-particulier";
+
 import TableauDeBordParticulier from "./inde/page";
 export default function Home() {
   return (
